bd7ac736be5b2a5abc4ead77ef080dd6b4ead4c6
Cao Jie <476624256@qq.com>
2015-12-25 14:04:31 +0000

    V3
    V3

--- isVisisted.m
+++ isVisisted.m
@@ -1,9 +1,12 @@
 function flag=isVisisted(nodeMove,visited)
-
+%{
+These function is uesd to estimate whether the node
+after moving is visited.
+%}
 flag=0;
-for i=1:length(visited)
+for i=1:length(visited)% loop: every member in visited
     if isequal(nodeMove.State,visited{1,i})
-        flag=flag+1;
+        flag=flag+1;% flag>0 means visited.
     end
 end
 
